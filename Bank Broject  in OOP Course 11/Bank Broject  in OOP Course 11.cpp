f62281587c7d853797843bdf7292819f82d86d04
alimou1sa <ali.mou1sa@gmail.com>
2024-07-18 10:30:36 +0300

    Add files via upload

---
+++ Bank Broject  in OOP Course 11/Bank Broject  in OOP Course 11.cpp
@@ -0,0 +1,26 @@
+
+// ProgrammingAdvices.com
+// Mohammed Abu-Hadhoud
+
+#include<iostream>
+#include"clsLoginScreen.h"
+using namespace std;
+
+int main()
+{
+
+
+		while (true)
+		{
+			if (!clsLoginScreen::ShowLoginScreen())
+			{
+				break;
+	     	}   
+		}
+
+
+		system("pause>0");
+			
+
+		return 0;
+}
